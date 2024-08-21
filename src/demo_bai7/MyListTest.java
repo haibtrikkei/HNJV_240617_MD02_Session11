1ac0800a6b45f2a1703439b35162f2ea2f05112a
haibtrikkei <buithanhhai9380@gmail.com>
2024-08-21 11:04:26 +0700

    Chua bai 7

---
+++ src/demo_bai7/MyListTest.java
@@ -0,0 +1,36 @@
+package demo_bai7;
+
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> list1 = new MyList<>();
+
+        list1.add(10);
+        for(int i=0;i< list1.size();i++){
+            System.out.println(list1.get(i));
+        }
+
+        list1.add(20);
+        System.out.println("\n");
+        for(int i=0;i< list1.size();i++){
+            System.out.println(list1.get(i));
+        }
+
+        list1.add(0, 30);
+        System.out.println("\n");
+        for(int i=0;i< list1.size();i++){
+            System.out.println(list1.get(i));
+        }
+
+        list1.remove(0);
+        System.out.println("\n");
+        for(int i=0;i< list1.size();i++){
+            System.out.println(list1.get(i));
+        }
+
+        list1.clear();
+        System.out.println("\nSau khi xoa het");
+        for(int i=0;i< list1.size();i++){
+            System.out.println(list1.get(i));
+        }
+    }
+}
